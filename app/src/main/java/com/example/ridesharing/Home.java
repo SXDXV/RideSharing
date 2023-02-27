3e1da76d7e2fadadf2569519c2718bcdece9edc3
Dmitry <shevtsov.dima.03@gmail.com>
2023-02-27 18:28:16 +0300

    Creating section fragments

--- app/src/main/java/com/example/ridesharing/Home.java
+++ app/src/main/java/com/example/ridesharing/Home.java
@@ -1,6 +1,9 @@
 package com.example.ridesharing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,6 +23,8 @@ public class Home extends AppCompatActivity {
 
         navigationView = findViewById(R.id.bottom_navigation);
         navigationView.getMenu().getItem(2).setChecked(true);
+
+        loadFragment(FragmentHomeTrips.newInstance());
     }
 
     @Override
@@ -32,10 +37,20 @@ public class Home extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem){
         switch (menuItem.getItemId()){
-//            case R.id.one:
-//                Toast toast = Toast.makeText(Home.this, "da", Toast.LENGTH_LONG);
-//                toast.show();
+            case R.id.men_search:
+            case R.id.men_publish:
+            case R.id.men_trips:
+                loadFragment(FragmentHomeTrips.newInstance());
+            case R.id.men_chat:
+            case R.id.men_profile:
         }
         return true;
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentHome,fragment);
+        transaction.commit();
     }
 }
